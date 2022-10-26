ddb9b51eabf4e43b2a18239f847eb6b616053302
Capsulers <113909207+Capsulers@users.noreply.github.com>
2022-10-26 11:37:21 +0800

    Update ffmpeg-nvenc-build.sh

--- ffmpeg-nvenc-build.sh
+++ ffmpeg-nvenc-build.sh
@@ -180,7 +180,7 @@ compileLibMP3Lame() {
 
 compileFfmpeg(){
     echo "Compiling ffmpeg"
-    Clone https://github.com/FFmpeg/FFmpeg -b master
+    Clone https://github.com/FFmpeg/FFmpeg -b release/4.4
 
     export PATH="$CUDA_DIR/bin:$PATH"  # ..path to nvcc
     PKG_CONFIG_PATH="$DEST_DIR/lib/pkgconfig:$DEST_DIR/lib64/pkgconfig" \
